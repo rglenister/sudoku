28aaf6ae138f87c0ef2d1e67499cf55057eee4c9
richard <rglenister@yahoo.com>
2024-02-05 14:53:18 +0000

    implemented Display for board

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 use std::collections::{BTreeSet, HashMap};
 use std::process::exit;
+use std::fmt;
 
 const NUMBER_OF_SQUARES: i32 = 81;
 const ROW_LENGTH: usize = 9;
 
+struct Board {
+    board: Vec<i32>,
+}
+
 fn main() {
+    // let puzzle_definition = {"
+    //     4--958--1
+    //     -7316254-
+    //     1--743-2-
+    //     3--21---5
+    //     71-39526-
+    //     2--48-1--
+    //     ---871-5-
+    //     -3752491-
+    //     5-1639--2
+    // "};
     let puzzle_definition = {"
-        4--958--1
-        -7316254-
-        1--743-2-
-        3--21---5
-        71-39526-
-        2--48-1--
-        ---871-5-
-        -3752491-
-        5-1639--2
+        -6-----8-
+        ---234--7
+        2-1------
+        ---5----1
+        --73--29-
+        4----7---
+        ----1-6--
+        5--879---
+        -8-------
     "};
 
     fn load_puzzle(definition: &str) -> Vec<i32> {
@@ -27,25 +43,25 @@ fn main() {
             .chars().flat_map(|ch| ch.to_digit(10).or_else(|| Some(0))).map(|i| i as i32).collect();
     }
 
-    let mut the_board: Vec<i32> = load_puzzle(puzzle_definition);
+    let mut the_board = Board { board: load_puzzle(puzzle_definition) };
     let square_to_squares_map= create_square_to_squares_map();
 
     println!("Puzzle definition:");
-    print_board(&the_board);
-    println!("\n{} solution(s) found", search(&mut the_board, &square_to_squares_map, 0, 0));
+    println!("{}", the_board);
+    println!("{} solution(s) found", search(&mut the_board, &square_to_squares_map, 0, 0));
 }
 
 // Brute force search that populates each empty square with all legal values
 fn search(
-    board: &mut Vec<i32>,
+    board: &mut Board,
     square_to_squares_map: &HashMap<i32, BTreeSet<i32>>,
     square_index: i32,
     num_solutions_found: i32
 ) -> i32 {
     match square_index {
         // a solution was found
-        NUMBER_OF_SQUARES => { print_board(board); return num_solutions_found + 1 }
-        _ => if board[square_index as usize] != 0 {
+        NUMBER_OF_SQUARES => { println!("{}", &board); return num_solutions_found + 1 }
+        _ => if board.board[square_index as usize] != 0 {
             // square already populated so move to the next one
             search(board, square_to_squares_map,square_index + 1, num_solutions_found)
         } else {
@@ -57,7 +73,7 @@ fn search(
 
 // Recursively populates the given square with each legal value and searches each resulting position
 fn fill_square_value(
-    board: &mut Vec<i32>,
+    board: &mut Board,
     square_to_squares_map: &HashMap<i32, BTreeSet<i32>>,
     square_index: i32,
     square_value: i32,
@@ -68,10 +84,10 @@ fn fill_square_value(
         num_solutions_found
     } else if is_legal_square_value(board, square_to_squares_map, square_index, square_value) {
         // populate the square and search the new position
-        board[square_index as usize] = square_value;
+        board.board[square_index as usize] = square_value;
         let temp_num_solutions = search(board, &square_to_squares_map, square_index + 1, num_solutions_found);
         let next_num_solutions_found: i32 = fill_square_value(board, square_to_squares_map, square_index, square_value + 1, temp_num_solutions);
-        board[square_index as usize] = 0;
+        board.board[square_index as usize] = 0;
         next_num_solutions_found
     } else {
         // try the next square value
@@ -81,29 +97,14 @@ fn fill_square_value(
 
 // Checks that the given square can be legally populated with the given value
 fn is_legal_square_value(
-    board: &mut Vec<i32>,
+    board: &mut Board,
     square_to_squares_map: &HashMap<i32, BTreeSet<i32>>,
     square_index: i32,
     square_value: i32
 ) -> bool {
     return square_to_squares_map.get(&square_index)
-        .map(|sqs| sqs.iter().map(|sq| board[*sq as usize])).expect("REASON")
+        .map(|sqs| sqs.iter().map(|sq| board.board[*sq as usize])).expect("REASON")
         .find(|val| val == &square_value).is_none();
-}
-
-fn print_board(
-    board: &Vec<i32>
-) {
-    let result: String = board.chunks(ROW_LENGTH)
-        .map(|line| line.iter()
-            .map(|item: &i32| { if item > &0 { item.to_string() } else { " ".to_string() }})
-            .collect::<Vec<_>>())
-            .collect::<Vec<_>>()
-            .iter().map(|line| line.join(" "))
-            .collect::<Vec<_>>()
-        .join("\n");
-    println!("{}", result);
-    println!();
 }
 
 // Creates a map from each board square to the set of squares whose value must be checked before the key square ios populated
@@ -122,4 +123,19 @@ fn create_square_to_squares_map() -> HashMap<i32, BTreeSet<i32>> {
         result.insert(square_index, square_set);
     }
     return result;
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let result: String = self.board.chunks(ROW_LENGTH)
+            .map(|line| line.iter()
+                .map(|item: &i32| { if item > &0 { item.to_string() } else { " ".to_string() }})
+                .collect::<Vec<_>>())
+            .collect::<Vec<_>>()
+            .iter().map(|line| line.join(" "))
+            .collect::<Vec<_>>()
+            .join("\n") + "\n";
+
+        return write!(f, "{}", result);
+    }
 }
